db684955bd958b36564eb7ae91bf6af985c00954
juanpcabana <juanpc3399@gmail.com>
2023-02-23 00:22:36 -0400

    fix de endpoint enrole para registrar alumnos viejos

--- components/user/controller.js
+++ components/user/controller.js
@@ -231,8 +231,7 @@ const addAvatar = async (tokenUser, file) => {
 
 }
 
-const enroleStudent = async (user, paymentInfo, tokenUser) => {
-
+const enroleStudent = async (user, paymentInfo, oldStudent, tokenUser) => {
     const existentUser = await store.findByEmail(user.email)
 
     let userId
@@ -241,21 +240,29 @@ const enroleStudent = async (user, paymentInfo, tokenUser) => {
         user.password = '123456'
         const userInfo = await addUser(user)
         userId = userInfo._id.toString()
-        await replaceUser(userId, { active: false, verifiedEmail: true, newStudent: false })
+        await replaceUser(userId, { active: true, verifiedEmail: true, newStudent: false })
 
     }
     else {
         userId = existentUser._id.toString()
-        await replaceUser(userId, { ...user, active: false, verifiedEmail: true, newStudent: false })
+        await replaceUser(userId, { ...user, active: true, verifiedEmail: true, newStudent: false })
     }
 
 
-    const newOrder = await orderController.inscription({ ammount: paymentInfo.bill, user: userId }, tokenUser.sub)
-    const orderId = newOrder._id.toString()
+    if (!oldStudent) {
+        const newOrder = await orderController.inscription({ ammount: paymentInfo.bill, user: userId }, tokenUser.sub)
+        const orderId = newOrder._id.toString()
 
-    const payment = await orderController.addPayment({ order: orderId, ...paymentInfo }, true)
+        const payment = await orderController.addPayment({ order: orderId, ...paymentInfo }, true)
 
-    return payment
+        return payment
+    }
+    else{
+        const response = await store.findById(userId, false)
+        const finalResponse = response.toObject()
+        delete finalResponse.password
+        return finalResponse
+    }
 }
 
 module.exports = {
