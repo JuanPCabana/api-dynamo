de3ca1f2603250cfba48055e02d4ccbc3536ab13
juanpcabana <juanpc3399@gmail.com>
2023-04-12 20:56:34 -0300

    test

--- components/helpers/membership/controller.js
+++ components/helpers/membership/controller.js
@@ -7,6 +7,7 @@ const OrderModel = require('../../order/model')
 const orderStore = require('../../order/store')
 const userController = require('../../user/controller')
 const orderController = require('../../order/controller')
+const UserModel = require('../../user/model')
 
 
 const verifyDebts = async () => {
@@ -54,29 +55,61 @@ const generateMonthlyBills = async () => {
     const nextPayment = `${day}/${nextMonth}`
 
 
-    const todayUserList = async () => { return await userStore.list() }
+    const todayUserList = async () => {
+        return await UserModel.aggregate([
+            {
+                $lookup: {
+                    from: 'orders',
+                    localField: '_id',
+                    foreignField: 'user',
+                    as: 'orders'
+                }
+            },
+            {
+                $match: {
+                    "orders.date": {
+                        $not: {
+                            $gte: new Date(new Date().getFullYear(), new Date().getMonth(), 1)
+                        }
+                    },
+                    role: 'student'
+                }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    email: 1,
+                    firstName: 1,
+                    lastName: 1,
+                    orders: 1
+                }
+            }
+        ])
+    }
 
     return new Promise(async (resolve, reject) => {
 
         const list = await todayUserList()
+        console.log("🚀 ~ file: controller.js:93 ~ returnnewPromise ~ list:", list)
+        return resolve({ length: list.length, a: list })
         // list.map(async (user) => {
-        const ordersGenerated = []
-        for (user of list.docs) {
-            if (user.status === 'active' || user.status === 'debtor' /* user._id.toString() === '641b759db70ef007f3117856' */) {
-                console.log("🚀 ~ list", user.email)
-                let auxUser = user.toObject()
+        // const ordersGenerated = []
+        // for (user of list.docs) {
+        //     if (user.status === 'active' || user.status === 'debtor' /* user._id.toString() === '641b759db70ef007f3117856' */) {
+        //         console.log("🚀 ~ list", user.email)
+        //         let auxUser = user.toObject()
 
-                orderController.add({ ammount: user?.membership?._id.toString() ?? '63c56873019597f1d03b24e2', user: auxUser._id })
+        //         orderController.add({ ammount: user?.membership?._id.toString() ?? '63c56873019597f1d03b24e2', user: auxUser._id })
 
-                // userController.replace(auxUser._id, { nextPaymentDate: nextPayment/* , active: false */ })
+        //         // userController.replace(auxUser._id, { nextPaymentDate: nextPayment/* , active: false */ })
 
-                await sendMailService.sendMailNewBill(user.email, user)
-                ordersGenerated.push(user)
-            }
-        }
-        resolve({
-            totalOrdersGenerated: ordersGenerated.length
-        })
+        //         await sendMailService.sendMailNewBill(user.email, user)
+        //         ordersGenerated.push(user)
+        //     }
+        // }
+        // resolve({
+        //     totalOrdersGenerated: ordersGenerated.length
+        // })
 
     })
 }
