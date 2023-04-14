3732eb457ac3fa801f073020fb3ccf2111eb356e
Juan Perez <juanpc3399@gmail.com>
2023-04-14 16:17:08 -0400

    cambios en agregacion de informacion por producto

--- components/helpers/dashboard/store.js
+++ components/helpers/dashboard/store.js
@@ -6,152 +6,249 @@ const { default: mongoose } = require('mongoose')
 
 const getCategoriesInfo = async () => {
 
-    const categoriesInfo = await CategoryModel.aggregate([
-        {
-            '$lookup': {
-                'from': 'users',
-                'localField': '_id',
-                'foreignField': 'category',
-                'as': 'users'
-            }
-        }
-    ])
-    const orderList = await OrderModel.find({ status: 'unpaid', inscription: false }).populate([{ path: 'user' }, { path: 'ammount' }])
+  const categoriesInfo = await CategoryModel.aggregate([
+    {
+      '$lookup': {
+        'from': 'users',
+        'localField': '_id',
+        'foreignField': 'category',
+        'as': 'users'
+      }
+    }
+  ])
+  const orderList = await OrderModel.find({ status: 'unpaid', inscription: false }).populate([{ path: 'user' }, { path: 'ammount' }])
 
 
-    const returnCategories = categoriesInfo.map(categoryInfo => {
-        let activeUsers = 0
-        let inactiveUsers = 0
-        let registeredUsers = 0
+  const returnCategories = categoriesInfo.map(categoryInfo => {
+    let activeUsers = 0
+    let inactiveUsers = 0
+    let registeredUsers = 0
 
-        categoryInfo.users.map(user => {
-            if (user.status === 'active') activeUsers = activeUsers + 1
-            if (user.status === 'inactive' || user.status === 'debtor') inactiveUsers = inactiveUsers + 1
-            if (user.status === 'registered') registeredUsers = registeredUsers + 1
-
-        })
-
-        let totalCategoryDebt = 0
-
-        orderList.map(order => {
-            if (order?.user?.category?.toString() === categoryInfo._id.toString()) {
-                totalCategoryDebt = totalCategoryDebt + order.ammount.ammount
-            }
-        })
-
-        return {
-            // ...categoryInfo,
-            _id: categoryInfo._id,
-            name: categoryInfo.name,
-            code: categoryInfo.code,
-            activeUsers,
-            inactiveUsers,
-            registeredUsers,
-            totalUsers: categoryInfo.users.length,
-            totalCategoryDebt
-        }
+    categoryInfo.users.map(user => {
+      if (user.status === 'active') activeUsers = activeUsers + 1
+      if (user.status === 'inactive' || user.status === 'debtor') inactiveUsers = inactiveUsers + 1
+      if (user.status === 'registered') registeredUsers = registeredUsers + 1
 
     })
 
-    const totalDebt = returnCategories.reduce((accumulator, currentValue) => accumulator + currentValue.totalCategoryDebt, 0);
+    let totalCategoryDebt = 0
+
+    orderList.map(order => {
+      if (order?.user?.category?.toString() === categoryInfo._id.toString()) {
+        totalCategoryDebt = totalCategoryDebt + order.ammount.ammount
+      }
+    })
 
     return {
-        docs: returnCategories,
-        totalDebt
-
+      // ...categoryInfo,
+      _id: categoryInfo._id,
+      name: categoryInfo.name,
+      code: categoryInfo.code,
+      activeUsers,
+      inactiveUsers,
+      registeredUsers,
+      totalUsers: categoryInfo.users.length,
+      totalCategoryDebt
     }
+
+  })
+
+  const totalDebt = returnCategories.reduce((accumulator, currentValue) => accumulator + currentValue.totalCategoryDebt, 0);
+
+  return {
+    docs: returnCategories,
+    totalDebt
+
+  }
 
 }
 
 const getCategoryInfo = async (id) => {
-    const userList = await UserModel.aggregate(
-        [
-            {
-                '$match': {
-                    'category': mongoose.Types.ObjectId(id),
-                    // 'status': {
-                    //     '$ne': 'registered'
-                    // }
-                }
-            },
-            {
-                '$lookup': {
-                    'from': 'orders',
-                    'localField': '_id',
-                    'foreignField': 'user',
-                    'as': 'unpaidOrders'
-                }
-            }, {
-                '$unwind': {
-                    'path': '$unpaidOrders'
-                }
-            }, {
-                '$match': {
-                    'unpaidOrders.status': 'unpaid'
-                }
-            }, {
-                '$lookup': {
-                    'from': 'prices',
-                    'localField': 'unpaidOrders.ammount',
-                    'foreignField': '_id',
-                    'as': 'productAmounts'
-                }
-            }, {
-                '$group': {
-                    '_id': '$_id',
-                    'user': {
-                        '$first': '$$ROOT'
-                    },
-                    'totalDebt': {
-                        '$sum': {
-                            '$arrayElemAt': [
-                                '$productAmounts.ammount', 0
-                            ]
-                        }
-                    },
-                    'orders': {
-                        '$push': '$unpaidOrders'
-                    }
-                }
-            }, {
-                '$unset': 'user.unpaidOrders'
-            }, {
-                '$unset': 'user.productAmounts'
-            }, {
-                '$lookup': {
-                    'from': 'categories',
-                    'localField': 'user.category',
-                    'foreignField': '_id',
-                    'as': 'user.category'
-                }
-            }, {
-                '$project': {
-                    '_id': 0,
-                    'category': '$user.category.name',
-                    'user': '$user',
-                    'orders': '$orders',
-                    'totalDebt': 1
-                }
+  const userList = await UserModel.aggregate(
+    [
+      {
+        '$match': {
+          'category': mongoose.Types.ObjectId(id),
+          // 'status': {
+          //     '$ne': 'registered'
+          // }
+        }
+      },
+      {
+        '$lookup': {
+          'from': 'orders',
+          'localField': '_id',
+          'foreignField': 'user',
+          'as': 'unpaidOrders'
+        }
+      }, {
+        '$unwind': {
+          'path': '$unpaidOrders'
+        }
+      }, {
+        '$match': {
+          'unpaidOrders.status': 'unpaid'
+        }
+      }, {
+        '$lookup': {
+          'from': 'prices',
+          'localField': 'unpaidOrders.ammount',
+          'foreignField': '_id',
+          'as': 'productAmounts'
+        }
+      }, {
+        '$group': {
+          '_id': '$_id',
+          'user': {
+            '$first': '$$ROOT'
+          },
+          'totalDebt': {
+            '$sum': {
+              '$arrayElemAt': [
+                '$productAmounts.ammount', 0
+              ]
             }
-        ]
-    );
-    const returnList = []
-    for (user of userList) {
-        const auxUser = user
-        delete auxUser.user.password
-        returnList.push(auxUser)
-    }
+          },
+          'orders': {
+            '$push': '$unpaidOrders'
+          }
+        }
+      }, {
+        '$unset': 'user.unpaidOrders'
+      }, {
+        '$unset': 'user.productAmounts'
+      }, {
+        '$lookup': {
+          'from': 'categories',
+          'localField': 'user.category',
+          'foreignField': '_id',
+          'as': 'user.category'
+        }
+      }, {
+        '$project': {
+          '_id': 0,
+          'category': '$user.category.name',
+          'user': '$user',
+          'orders': '$orders',
+          'totalDebt': 1
+        }
+      }
+    ]
+  );
+  const returnList = []
+  for (user of userList) {
+    const auxUser = user
+    delete auxUser.user.password
+    returnList.push(auxUser)
+  }
 
-    const totalDebt = returnList.reduce((accumulator, currentValue) => accumulator + currentValue.totalDebt, 0);
+  const totalDebt = returnList.reduce((accumulator, currentValue) => accumulator + currentValue.totalDebt, 0);
 
 
-    return { docs: returnList, totalCategoryDebt: totalDebt }
+  return { docs: returnList, totalCategoryDebt: totalDebt }
 
 }
 
 
-const perProductInfo = async () =>{
-    return await OrderModel.aggregate([
+const perProductInfo = async () => {
+  const paymentInfoDetails = await OrderModel.aggregate(
+    [
+      {
+        '$lookup': {
+          'from': 'users',
+          'localField': 'user',
+          'foreignField': '_id',
+          'as': 'user'
+        }
+      }, {
+        '$match': {
+          'status': 'approved',
+          'inscription': false
+        }
+      }, {
+        '$lookup': {
+          'from': 'categories',
+          'localField': 'user.category',
+          'foreignField': '_id',
+          'as': 'category'
+        }
+      }, {
+        '$lookup': {
+          'from': 'prices',
+          'localField': 'ammount',
+          'foreignField': '_id',
+          'as': 'membership'
+        }
+      }, {
+        '$unwind': {
+          'path': '$user'
+        }
+      }, {
+        '$unwind': {
+          'path': '$category'
+        }
+      }, {
+        '$unwind': {
+          'path': '$membership'
+        }
+      }, {
+        '$group': {
+          '_id': {
+            'method': '$payment.method',
+            'category': {
+              'name': '$category.name',
+              '_id': '$category._id'
+            },
+            'membership': {
+              '_id': '$membership._id',
+              'name': '$membership.name'
+            }
+          },
+          'totalAmmount': {
+            '$sum': '$membership.ammount'
+          },
+          'orders': {
+            '$push': '$$ROOT'
+          }
+        }
+      }, {
+        '$group': {
+          '_id': {
+            'method': '$_id.method',
+            'membership': {
+              '_id': '$_id.membership._id',
+              'name': '$_id.membership.name'
+            }
+          },
+          'totalProductAmmount': {
+            '$sum': '$totalAmmount'
+          },
+          'orders': {
+            '$push': '$orders'
+          }
+        }
+      }, {
+        '$group': {
+          '_id': '$_id.method',
+          'totalAmmount': {
+            '$sum': '$totalProductAmmount'
+          },
+          'methods': {
+            '$push': '$$ROOT'
+          }
+        }
+      }, {
+        '$project': {
+          '_id': 1,
+          'totalAmmount': 1,
+          'methods': {
+            '_id': 1,
+            'totalProductAmmount': 1
+          }
+        }
+      }
+    ]
+      /* [
         {
           '$lookup': {
             'from': 'prices', 
@@ -212,14 +309,22 @@ const perProductInfo = async () =>{
             }
           }
         }
-      ])
+      ] */)
 
-      
+
+  return {
+    docs: [
+      ...paymentInfoDetails,
+     
+    ]
+  }
+
+
 }
 
 
 module.exports = {
-    getCategoriesInfo,
-    getCategoryInfo,
-    perProductInfo
+  getCategoriesInfo,
+  getCategoryInfo,
+  perProductInfo
 }
